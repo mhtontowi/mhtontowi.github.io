63f96604ddb8f21d063603fa3771ac3653a30ed1
mhtontowi <52361221+mhtontowi@users.noreply.github.com>
2022-04-14 23:55:17 +0200

    Add files via upload

---
+++ assets/js/typed-text.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.type', {
+    strings: [  'Graphic Designer', 
+                'UI Designer',
+                'Web Developer'],
+    
+    typeSpeed: 30,
+    backSpeed: 7.5,
+    loop: true // Default value
+    });
