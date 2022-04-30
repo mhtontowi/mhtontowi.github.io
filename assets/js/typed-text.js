aa99f7f64de2d50d37eaa83e0431fc4e9a96cd23
mhtontowi <52361221+mhtontowi@users.noreply.github.com>
2022-04-30 04:13:09 +0200

    Delete assets directory

--- assets/js/typed-text.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed('.type', {
-    strings: [  'Graphic Designer', 
-                'UI Designer',
-                'Web Developer'],
-    
-    typeSpeed: 30,
-    backSpeed: 7.5,
-    loop: true // Default value
-    });
